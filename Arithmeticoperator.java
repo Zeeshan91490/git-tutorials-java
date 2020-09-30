31b6520795e489b2680246b65c7f6bff7046f0aa
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-09-30 10:46:43 +0530

    Arithmeticoperator.java

---
+++ Arithmeticoperator.java
@@ -0,0 +1,13 @@
+package javtutorials;
+
+public class Arithmeticoperator {
+
+	public static void main(String[] args){
+//Arithematic operators are +,-,*,/
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		
+	}
+
+}
